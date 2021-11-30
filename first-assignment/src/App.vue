eab5824552ce92969939937a979aaf4e6498f03d
Tyler Boskay <tylerboskay@gmail.com>
2021-11-30 10:05:16 -0600

    Scaffolding a Component

---
+++ first-assignment/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="app">
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+                friends: [
+                    {
+                        id: 'tyler',
+                        name: 'Tyler',
+                        phone: '555-555-5555',
+                        email: 'tyler@localhost.com'
+                    },
+                    {
+                        id: 'joe',
+                        name: 'Joe',
+                        phone: '555-555-5555',
+                        email: 'joe@localhost.com'
+                    },
+                ]
+            }
+        }
+    }
+</script>
---
+++ first-assignment/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                name: 'Tyler',
+                phone: '555-555-5555',
+                email: 'tyler@test.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.showDetails = !this.showDetails;
+        },
+    }
+};
+</script>
