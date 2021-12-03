4b7010fda12ee70d0955097f7df571837bfa48df
Tyler Boskay <tylerboskay@gmail.com>
2021-12-03 13:25:24 -0600

    Working with Props.

--- component-communication/src/App.vue
+++ component-communication/src/App.vue
@@ -4,8 +4,14 @@
       <h1>My Friends</h1>
     </header>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+      v-for="friend in friends"
+      :key="friend.id" 
+      :name="friend.name" 
+      :phone-number="friend.phone" 
+      :email-address="friend.email"
+      is-favorite="1"
+      ></friend-contact>
     </ul>
   </section>
 </template>
